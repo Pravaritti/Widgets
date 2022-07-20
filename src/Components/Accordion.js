b89db2859fad97ad56770794ab9c9f6053371063
Pravaritti <pravaritti.1995@gmail.com>
2022-07-20 15:32:39 +0530

    add helper function to function component

--- src/Components/Accordion.js
+++ src/Components/Accordion.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 const Accordion = ({ items }) => {
-  const renderedItems = items.map((item) => {
+  const onTitleClick = (index) => {
+    console.log("Title clicked", index);
+  };
+
+  const renderedItems = items.map((item, index) => {
     return (
       <React.Fragment key={item.title}>
         {/* to avoid extra border but still add a key, we use react.fragment instead of div */}
-        <div className="title active">
+        <div className="title active" onClick={() => onTitleClick(index)}>
+          {/*why arrow function? because we do not want to run that callback function the instant our componenet is rederded. instead we want to call it at som epoint in the future.*/}
           <i className="dropdown icon"></i>
           {item.title}
         </div>
