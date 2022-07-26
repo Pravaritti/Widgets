4547e97ae595c49b137e67b50acbaab57d39306f
Pravaritti <pravaritti.1995@gmail.com>
2022-07-26 15:13:37 +0530

    implement useEffect hook

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,7 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Search = () => {
   const [term, setTerm] = useState("");
+
+  console.log("I run with every render.");
+
+  useEffect(() => {
+    console.log("I run after every render and at first render");
+  }, [term]); //2nd argument = when our code is executed (3 scenerios in point 5 of hook system)
+  //options to pass
+  //1. empty array = []
+  //2. array with one or more elements inside it = [term]
+  //3. pass nothing at all
 
   return (
     <div>
