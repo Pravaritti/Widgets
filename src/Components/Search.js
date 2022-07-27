7edd7b47d2b1e1e8f37537a241899fb8276c4b59
Pravaritti <pravaritti.1995@gmail.com>
2022-07-27 13:36:32 +0530

    make API call to wiki using axios

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,17 +1,23 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("");
 
-  console.log("I run with every render.");
-
   useEffect(() => {
-    console.log("I run after every render and at first render");
-  }, [term]); //2nd argument = when our code is executed (3 scenerios in point 5 of hook system)
-  //options to pass
-  //1. empty array = []
-  //2. array with one or more elements inside it = [term]
-  //3. pass nothing at all
+    const search = async () => {
+      await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+    };
+    search();
+  }, [term]);
 
   return (
     <div>
