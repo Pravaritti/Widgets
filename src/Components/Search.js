3589869ba20f9b3c455dcdcfbad5c3f4092f7f99
Pravaritti <pravaritti.1995@gmail.com>
2022-07-28 16:57:50 +0530

    add default search term

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Search = () => {
-  const [term, setTerm] = useState("");
+  const [term, setTerm] = useState("programming"); //have to put default search term otherwise it will throw an error because it will be grenderingat empty string.
+  const [results, setResults] = useState([]);
+
+  //console.log(results);
 
   useEffect(() => {
     const search = async () => {
-      await axios.get("https://en.wikipedia.org/w/api.php", {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
         params: {
           action: "query",
           list: "search",
@@ -15,6 +18,8 @@ const Search = () => {
           srsearch: term,
         },
       });
+
+      setResults(data); // we set result with data returned by axios' get request and it causes our component to rerender.
     };
     search();
   }, [term]);
