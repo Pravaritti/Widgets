41aeafc47a4c4214c45fe186f43b81df60556ad5
Pravaritti <pravaritti.1995@gmail.com>
2022-08-08 18:18:14 +0530

    debounce translation updates

--- src/Components/Convert.js
+++ src/Components/Convert.js
@@ -3,6 +3,20 @@ import axios from "axios";
 
 const Convert = ({ language, text }) => {
   const [translated, setTranslated] = useState("");
+  const [debouncedText, setDebouncedText] = useState(text);
+
+  //debouncing because google translate api is a paid api and at every keypress, we make a request and every request costs us.
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedText(text);
+    }, 500);
+
+    //return cleanup function
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [text]);
+
   useEffect(() => {
     //console.log("New Language or text");
 
@@ -13,7 +27,7 @@ const Convert = ({ language, text }) => {
         {},
         {
           params: {
-            q: text,
+            q: debouncedText,
             target: language.value,
             key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
             //this key is for google api which is paid but this is guven in the course and only works for localhost3000
@@ -27,7 +41,7 @@ const Convert = ({ language, text }) => {
     };
 
     doTranslation();
-  }, [language, text]);
+  }, [language, debouncedText]);
 
   return (
     <div>
